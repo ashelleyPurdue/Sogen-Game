d01caa151da6449a14dc335dd95803d7cf781bc7
Alex <ashelley@purdue.edu>
2015-01-26 14:49:44 -0500

    Added arrows and arrow shooters. Introduced arrows and arrow shooters in the level.

---
+++ Assets/Scripts/MonoBehaviors/ArrowBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowBehavior : MonoBehaviour
+{
+    public float lifetime = 10f;    //How long the arrow "lives" for before it is destroyed.
+    
+    //Events
+    
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        
+        if (lifetime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //TODO: Stick into the surface and fade out.
+        
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/MonoBehaviors/Level Objects/ArrowShooterBehavior.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowShooterBehavior : MonoBehaviour
+{
+    
+    public Transform arrowPoint;
+ 
+    public float waitTime = 3f;
+    public float warningTime = 0.5f;
+    
+    public float arrowSpeed = 10f;
+    
+    public enum State {waiting, warning};
+    private State currentState;
+    
+    private ArrowBehavior myArrow = null;
+    
+    private float timer = 0f;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (currentState == State.waiting)
+        {
+            //Start warning after a bit
+            
+            myArrow = null;
+            
+            timer += Time.deltaTime;
+            if (timer >= waitTime)
+            {
+                //Move to the next state
+                timer -= waitTime;
+                currentState = State.warning;
+                
+                //Create the arrow
+                myArrow = ((GameObject)Instantiate(Resources.Load("arrow_prefab"))).GetComponent<ArrowBehavior>();
+                myArrow.transform.position = arrowPoint.position;
+            }
+        }
+        else if (currentState == State.warning)
+        {
+            //After warning for a bit, fire.
+            
+            timer += Time.deltaTime;
+            if (timer >= warningTime)
+            {
+                //Fire
+                myArrow.rigidbody2D.velocity = arrowSpeed * new Vector2(1, 0);
+                
+                //Move to the next state
+                timer = 0f;
+                currentState = State.waiting;
+            }
+        }
+	}
+}
