f9e85f9fe461daa7ff6a9a4ada54555c0c17760a
Alex <ashelley@purdue.edu>
2015-02-26 12:54:38 -0500

    Locks now change size depending on how many keys are left.

--- Assets/Scripts/MonoBehaviors/Level Objects/LockBehavior.cs
+++ Assets/Scripts/MonoBehaviors/Level Objects/LockBehavior.cs
@@ -4,25 +4,60 @@ using System.Collections.Generic;
 
 public class LockBehavior : CircuitNodePowerSource
 {
+    private const float SHRINK_SPEED = 2f;
+    private const float BASE_SCALE = 1f;
+    private const float SCALE_PER_KEY = 0.5f;
+    
     private int keysLeft = 0;
+    private int maxKeys = 0;
+    
+    private float targetScale = 1f;
+    
+    //Events
+    
+    void Start()
+    {
+        maxKeys = keysLeft;
+        UpdateStatus();
+    }
+    
+    void Update()
+    {
+        float scale = transform.localScale.x;
+        
+        scale = Mathf.MoveTowards(scale, targetScale, SHRINK_SPEED * Time.deltaTime);
+        
+        transform.localScale = new Vector3(scale, scale, 0);
+    }
     
     //Interface
     public void AddKey()
     {
         keysLeft++;
-        UpdatePower();
+        UpdateStatus();
     }
     
     public void RemoveKey()
     {
         keysLeft--;
-        UpdatePower();
+        UpdateStatus();
     }
     
     //Misc methods
     
-    private void UpdatePower()
+    private void UpdateStatus()
     {
+        //Update the size of the lock
+        if (keysLeft > 0)
+        {
+            targetScale = BASE_SCALE + SCALE_PER_KEY * (keysLeft - 1);
+        }
+        else
+        {
+            targetScale = 0f;
+        }
+        
+        //Update the circuitry
         isEnabled = keysLeft <= 0;
         UpdateCircuitry();
     }
