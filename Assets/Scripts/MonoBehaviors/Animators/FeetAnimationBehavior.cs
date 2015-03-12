ede93e6c4e4f61f379f6a575f4dcb77b63762613
Alex <ashelley@purdue.edu>
2015-03-12 14:48:58 -0400

    Made the player's feet animate.
    Something strange happened with all those unrelated files being included
    	in the commit.  Not sure what.

---
+++ Assets/Scripts/MonoBehaviors/Animators/FeetAnimationBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeetAnimationBehavior : MonoBehaviour
+{
+    public Transform leftFoot;
+    public Transform rightFoot;
+    
+    public float stepSize = 1f;
+    
+    public float footLevel = -1f;   //How far down the feet are.
+    public float horOffset = 0.25f;
+    
+    public PlatformCharacterMotor motorToWatch;     //The feet will animate in accordance with the motion of this motor.
+    
+    
+    private Vector3 leftTargPos;
+    private Vector3 rightTargPos;
+    
+    private float timer = 0f;
+    
+    //Events
+    
+    void Awake()
+    {
+        //Start the target positions.
+        leftTargPos = new Vector3(horOffset, footLevel, 0);
+        rightTargPos = new Vector3(horOffset, footLevel, 0);
+    }
+    
+    void Update()
+    {   
+        //Move the feet to their target positions.
+        leftFoot.localPosition = Vector3.MoveTowards(leftFoot.localPosition, leftTargPos, 10 * Time.deltaTime);
+        rightFoot.localPosition = Vector3.MoveTowards(rightFoot.localPosition, rightTargPos, 10 * Time.deltaTime);
+        
+        //Move the feet
+        timer += motorToWatch.rigidbody2D.velocity.x * Time.deltaTime / stepSize;
+
+        leftTargPos.x = horOffset + Mathf.Sin(timer) * stepSize;
+        rightTargPos.x = horOffset -1 * Mathf.Sin(timer + (Mathf.PI/8)) * stepSize;
+        
+
+    }
+}
---
+++ Assets/Scripts/MonoBehaviors/QuickObjects/QuickRectangle.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class QuickRectangle : QuickObject
+{
+    public Vector3 pointA = new Vector3(0, 1, 0);
+    public Vector3 pointB = new Vector3(1, 0, 0);
+
+    private BoxCollider2D boxCol;
+
+    //Misc methods
+
+    protected override void CreateMesh()
+    {
+        //Create the filter's mesh if it doesn't exist.
+        if (filter == null || filter.sharedMesh == null)
+        {
+            filter = GetComponent<MeshFilter>();
+            filter.sharedMesh = new Mesh();
+            filter.sharedMesh.vertices = new Vector3[4];
+        }
+        
+        //Create the vertices
+        Vector3[] vertices = new Vector3[4];
+        
+        vertices [0] = pointA;
+        vertices [1] = new Vector3(pointB.x, pointA.y);
+        vertices [2] = pointB;
+        vertices [3] = new Vector3(pointA.x, pointB.y);
+        
+        //Update the mesh
+        filter.sharedMesh.Clear();
+        filter.sharedMesh.vertices = vertices;
+        
+        filter.sharedMesh.triangles = new int[] {0, 1, 3, 2, 3, 1};
+        filter.sharedMesh.RecalculateNormals();
+    }
+
+    protected override void UpdateCollider()
+    {
+        //Update the collider
+        boxCol = GetComponent<BoxCollider2D>();
+        
+        boxCol.center = (pointA + pointB) / 2;
+        boxCol.size = new Vector2(Mathf.Abs(pointA.x - pointB.x), Mathf.Abs(pointA.y - pointB.y));
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(QuickRectangle))]
+public class QuickRectangleEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        QuickRectangle recTarget = (QuickRectangle)target;
+
+        Vector3 rectPos = recTarget.transform.position;
+
+        recTarget.pointA = Handles.PositionHandle(rectPos + recTarget.pointA, Quaternion.identity) - rectPos;
+        recTarget.pointB = Handles.PositionHandle(rectPos + recTarget.pointB, Quaternion.identity) - rectPos;
+
+        recTarget.UpdateMesh();
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
+#endif
