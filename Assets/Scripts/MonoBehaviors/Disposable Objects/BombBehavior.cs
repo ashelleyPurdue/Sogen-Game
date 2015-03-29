536c3efe8db17323f1f306b6fff38a3e7ea28898
Alex <ashelley@purdue.edu>
2015-03-29 16:17:34 -0400

    Bombs now light their fuse when picked up.  When the fuse runs out, they will 	disappear.  They still don't explode.

---
+++ Assets/Scripts/MonoBehaviors/Disposable Objects/BombBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ThrowableBehavior))]
+
+public class BombBehavior : MonoBehaviour 
+{
+    public float timeLeft = 1f;
+    
+    public ParticleSystem myParticles;
+    
+    public bool fuseLit = false;
+    
+    //Events
+    
+    void OnPickedUp()
+    {
+        fuseLit = true;
+    }
+    
+    void Update()
+    {            
+        if (fuseLit)
+        {
+            //Count down
+            timeLeft -= Time.deltaTime;
+            
+            if (timeLeft <= 0)
+            {
+                Explode();
+            }
+        }
+        
+        //Show/hide the fuse
+        myParticles.enableEmission = fuseLit;
+    }
+    
+    //Misc methods
+    
+    private void Explode()
+    {
+        //TODO: Create explosion
+        GameObject.Destroy(gameObject);
+    }
+}
