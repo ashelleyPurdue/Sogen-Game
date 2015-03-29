2ddf48a755ae390a88095832655c97ffb8105429
Alex <ashelley@purdue.edu>
2015-03-29 17:18:34 -0400

    Bombs now flash before exploding, as a warning.

--- Assets/Scripts/MonoBehaviors/Disposable Objects/BombBehavior.cs
+++ Assets/Scripts/MonoBehaviors/Disposable Objects/BombBehavior.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 
 [RequireComponent(typeof(ThrowableBehavior))]
+[RequireComponent(typeof(SpriteRenderer))]
 
 public class BombBehavior : MonoBehaviour 
 {
+    public const float WARNING_TIME = 1.5f;
+    public const float PULSE_FREQUENCY = 10;
+    
     public float timeLeft = 1f;
     
     public ParticleSystem myParticles;
     
     public bool fuseLit = false;
     
+    private SpriteRenderer sprRend;
+    
     //Events
+    
+    void Awake()
+    {
+        sprRend = GetComponent<SpriteRenderer>();
+    }
     
     void OnPickedUp()
     {
@@ -19,12 +30,20 @@ public class BombBehavior : MonoBehaviour
     }
     
     void Update()
-    {            
+    {     
+        //Count down while fuse is lit.
         if (fuseLit)
         {
             //Count down
             timeLeft -= Time.deltaTime;
             
+            //Warn when time is running out.
+            if (timeLeft <= WARNING_TIME)
+            {
+                sprRend.color = Color.Lerp(Color.white, Color.red, Mathf.PingPong(timeLeft * PULSE_FREQUENCY, 1f) );
+            }
+            
+            //Explode when time is up
             if (timeLeft <= 0)
             {
                 Explode();
